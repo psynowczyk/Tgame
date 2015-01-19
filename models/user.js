17c3157a24c207d5fe5f8142a86cb7f55decdb5d
Piotr Synowczyk <piotrsynowczyk@wp.pl>
2015-01-19 16:52:22 +0100

    added User model

---
+++ models/user.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+var userSchema = mongoose.Schema({
+   local : {
+      'login': String,
+      'password': String,
+      'username': String,
+      'usertype': {type: String, default: 'user'},
+      'acstatus': {type: String, default: 'useable'},
+      'avatar': String,
+      'territory': {type: Number, default: 0},
+      'registerdate': {type: Date, default: Date.now()}
+   }
+});
+
+// generating a hash
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('User', userSchema);
