d736f190a02d22bc5c859c73c18161e2c78dd074
Piotr Synowczyk <piotrsynowczyk@wp.pl>
2015-01-20 13:43:54 +0100

    layout changes

---
+++ public/javascripts/index.js
@@ -0,0 +1,14 @@
+var site = window.location.pathname;
+
+$(document).ready(function() {
+
+	var window_height = $(window).height();
+	var window_width = $(window).width();
+	$('.container').css({'width': (window_width-200) +'px', 'height': (window_height-200) +'px'});
+
+	$('a.navilink[href="'+ site +'"]').css({'background-color': '#0d5980', 'color': '#ffffff', 'box-shadow': 'inset 0px 3px 4px 0px #0a4766'});
+
+	if(site == '/') {
+
+	}
+});
--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,34 @@
 module.exports = function (app, passport) {
 
-	app.get('/', function(req, res, next) {
-	  res.render('index', { title: 'Express' });
+	app.get('*', function (req, res, next) {
+		res.locals.loggedIn = (req.user) ? true : false;
+		if (req.isAuthenticated()) res.locals.username = req.user.local.username || null;
+		if (req.isAuthenticated()) res.locals.usertype = req.user.local.usertype || null;
+		next();
 	});
 
-	app.post('/login', passport.authenticate('local', {
-		successRedirect: '/',
-		failureRedirect: '/login'
+	// INDEX
+	app.get('/', function (req, res, next) {
+	  res.render('index', {title: 'Tgame'});
+	});
+
+	// SIGNUP
+	app.get('/signup', isLoggedOut, function (req, res) {
+		res.render('signup', {'title': 'Rejestracja'});
+	});
+	// SIGNUP
+	app.post('/signup', isLoggedOut, passport.authenticate('local-signup', {
+		successRedirect : '/',
+		failureRedirect : '/signup'
 	}));
 
+}
+
+function isLoggedIn(req, res, next) {
+	if (req.isAuthenticated()) return next();
+	res.redirect('/');
+}
+function isLoggedOut(req, res, next) {
+	if (req.isAuthenticated()) res.redirect('/');
+	return next();
 }
