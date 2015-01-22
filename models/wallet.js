9caeb145f794dc65b30e5e50505cb87dccdf9486
Piotr Synowczyk <piotrsynowczyk@wp.pl>
2015-01-22 21:19:57 +0100

    dodałem portfel użytkownika

--- models/wallet.js
+++ models/wallet.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
 
 var walletSchema = mongoose.Schema({
-      'owner': String,
-      'value': Number
+      'owner': mongoose.Schema.Types.ObjectId,
+      'value': {type: Number, default: 10000}
 });
 
 module.exports = mongoose.model('Wallet', walletSchema);
